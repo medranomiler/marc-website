b7ac0d480f10d4ee6c686d43456bfa2bb5c04e1e
Medranomiler1 <medranomiler@gmail.com>
2023-04-19 00:41:12 -0500

    yy

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -110,6 +110,7 @@ export default function Navbar() {
                   <Link
                     key={item.name}
                     href={item.href}
+                    onClick={() => setMobileMenuOpen(false)}
                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   >
                     {item.name}
