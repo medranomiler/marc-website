a99ef63f6c00d87b97349fc2235d80e3f652a176
Medranomiler1 <medranomiler@gmail.com>
2023-04-21 22:42:08 -0500

    collab and home pages

--- src/pages/collaborate.js
+++ src/pages/collaborate.js
@@ -3,8 +3,34 @@ import React from 'react'
 
 const Collaborate = () => {
   return (
-    <div className="max-w-screen py-36 p-4 relative">
-<div className="absolute top-0 left-0 w-full h-full bg-[url('../../public/genesisColored.svg')] opacity-[0.05] bg-image object-cover"/>
+    <div className="max-w-screen p-4 py-36 flex flex-col items-center relative lg:m-16 aboutMe">
+      <div className="flex flex-col items-center mx-auto w-full md:w-1/2 xl:w-1/3 mb-24">
+          <h1 className="font-bold text-3xl sm:text-4xl mb-16">
+            Collaborate
+          </h1>
+          <p className="leading-8">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae, odit vel quos et vitae praesentium suscipit cupiditate magni repellendus, laboriosam at ratione quas saepe rem expedita assumenda voluptatum itaque voluptatem?
+          </p>
+        <div className="mb-4">
+          <p className="leading-8">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae, odit vel quos et vitae praesentium suscipit cupiditate magni repellendus, laboriosam at ratione quas saepe rem expedita assumenda voluptatum itaque voluptatem?
+          </p>
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center mx-auto w-full md:w-1/2 xl:w-1/3 mb-24">
+        <h1 className="font-bold text-2xl sm:text-3xl mb-16">
+            Sub Header
+          </h1>
+          <p className="leading-8">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae, odit vel quos et vitae praesentium suscipit cupiditate magni repellendus, laboriosam at ratione quas saepe rem expedita assumenda voluptatum itaque voluptatem?
+          </p>
+        <div className="mb-4">
+          <p className="leading-8">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae, odit vel quos et vitae praesentium suscipit cupiditate magni repellendus, laboriosam at ratione quas saepe rem expedita assumenda voluptatum itaque voluptatem?
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 
 
@@ -7,14 +6,25 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>MARC MACKIRAHAN</title>
-        <meta name="description" content="My Personal Website" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
+    <div className="max-w-screen p-4 pt-12 flex flex-col items-center relative lg:m-10 aboutMe">
+      <div className="flex flex-col items-center mx-auto w-full md:w-1/2 xl:w-1/3 mb-24">
+          <h2 className="font-bold text-2xl sm:text-3xl mb-4">
+            Hello there!
+          </h2>
+          <h1 className="font-bold text-3xl sm:text-4xl mb-4">
+            My name is Marc
+          </h1>
+          <p className="leading-8">I am a perpetual student in the game of life.
+            <br/>
+             I like doing things. Mostly things that have to do with 
+             bitcoin, technology, and finance.
+             Peek around to learn more about me.
+             <br/>
+             Keep running. 
+          </p>
+          <img src="https://pbs.twimg.com/media/FlEA8bAWYAEO5QI.jpg" />
+      </div>
     </div>
   )
 }
+
