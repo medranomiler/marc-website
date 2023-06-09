c6cd5df42880bd7c6338c566de796ec35d186242
marc <wade.mckirahan@gmail.com>
2023-06-08 19:06:30 -0500

    website update

--- src/pages/collaborate.js
+++ src/pages/collaborate.js
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import keeprunning from "../images/keeprunning.png"
+import Image from "next/image"
 
 const Collaborate = () => {
   return (
@@ -16,7 +17,7 @@ const Collaborate = () => {
   <a href="mailto:marcmckirahan@gmail.com" class="underline-link text-slate-900"> Email</a>.
 </p>
 <div className="mx-auto w-full flex flex-col items-center">
-            <img src="https://lh3.googleusercontent.com/pw/AJFCJaV8EpXi50fxQMM337KouO3T_EieAW8FQmH1gcLXmKzolpGg4fHRcxOJskNENluTUcumF7nh8yZ9GPDeEKNZUrKUS-Ztc9QsPKm2_L6BLSm4v96ndaQh1eCvbr-k384Wdt5T_G7FF99wBi4-xCGuBe1lJH0hPGfYz7Xr7fYoskDDA1scuXYkicOxLq3sfxpe-c-O-v2eFtnUVZ9G5yygKyJroKPEV-Yk2KNqZgzlEJ7G9pK56wpng4NGIKHEbPBmCAKWQQZ5UKRmlUlw68jMhU6XN-r-20n9FszKfQNx2QaoruIUTozW7FFkfgIiWXQaQi4GmwVSTzY0KGbqG1YbrfpvocHwN4bOJW27ToadjjLxQZmAJY2h2AVtja97BSJaL8BW_Ztc6W9fcH6S7L95SCVcsr_SgPp6renJeWTAksik-shkaOUcPfO5BlOuDBXU8244HQQ5whp_I9CevVvboVSTtTL6hdo-AmgcyV-lX3b60gjzh2XERF_r3SWPfaP_mkOr9WrvumsIL1RL4-ZU5UItGVkEcfESnNf4kqWRdNUtsMae_L9i-mFvZgk6aRvM-e1jdtZFa5lCRx69NCUo-L99OKq_0a36nHMNX69HGWhEtFoLRnfOBtDbvdTKuPaYQ-ZYRO_qOqMKu91kaC0QQ847kJlgwsM3lrzrLL2hSecQ3gkc4_mAAuU_KQqFPiUjOCe43Tu23qg0k3zUCnjCmUD7zzHl0C_9wkT_RUNwVAjv3hzjsUYbTaXMdDVhouLJ4ALghYDU5tLjafaQM8nRwlZFqsNGWfn3-UjQooJsjlsZYzufge6QZSqVblfmKFFMa5BrB-JCGtbMbLYVD4tHCgiQfcrAwui835A0TZ-7pHARcs263kGxGVmQJZmd5KsKRsPZzPVYHyMdccF-uGkuFXDK9PYsnA4BZgSFOzTk7_UT3G-OFZwFoSXYWd2l-mRy1ZBnney7XfTNegeZCVO5EvdoWkJl6Q=w572-h681-s-no?authuser=0" />
+            <Image src={keeprunning} />
             <figcaption className="font-light italic text-slate-600">...keep running</figcaption>
   </div>
         <div className="mb-4">
