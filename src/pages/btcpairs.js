dda12cb86304c0c92d45a9e8232a3171e60192f4
marc <wade.mckirahan@gmail.com>
2023-07-24 21:58:36 -0500

    website update

--- src/pages/btcpairs.js
+++ src/pages/btcpairs.js
@@ -11,16 +11,6 @@ const Btcpairs = () => {
         <p>DASHBOARD UNDER CONTRUCTION</p>
         <br/>
         <br/>
-        <!--
-        <p>1D</p>
-        <p>5D</p>
-        <p>1M</p>
-        <p>6M</p>
-        <p>YTD</p>
-        <p>1Y</p>
-        <p>5Y</p>
-        <p>Max</p>
-        -->
         </div> 
       <CurrencyCards />
       {/* <div className="max-w-[800px] h-12 flex space-x-4 py-3 px-6 mb-4 text-black">
