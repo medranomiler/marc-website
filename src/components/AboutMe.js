b7a785a05a29047ee48b7bf2f6f734962419f35e
marc <wade.mckirahan@gmail.com>
2023-10-09 23:13:10 -0500

    website update

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -42,7 +42,7 @@ const AboutMe = () => {
         The room fell silent and as I mustered up the courage to confess, I leaned in wearing my finest deadpan expression (IYKYK) and responded with, ‘talking about myself’. Cue the collective sigh of relief from the crowd, that my long-held oversharing tendencies were finally out in the open.        
         <br/>
         <br />
-        I find more interest in the things that aren't said, rather than the things that are. Consistent with my strive to influence, and be influenced by actions, rather than words. I have always been more of a "let me show you, not tell you" kinda guy. So here we are, sitting at the black-hole of my ‘about me’ bio and yet another attempt at weaving an intriguing narrative about my identity. 
+        I often find more interest in the things that aren't said, rather than the things that are. Consistent with my strive to influence, and be influenced by actions, rather than words. I have always been more of a show you, not tell you kinda guy. So here we are, sitting at the black-hole of my ‘about me’ bio and yet another attempt at weaving an intriguing narrative about my identity. 
         <br/>
         <br />   
         The end.
@@ -67,7 +67,6 @@ const AboutMe = () => {
             Why Am I Here            
             </h1>
             <p className="leading-8 text-slate-700">
-            Rewinding back to that enchanting memory of my kindergarten circus, which I know you enjoyed dearly, I discovered the freedom to choose privacy. Regrettably, in today's world, we find ourselves amidst the erosion of monetary and property rights, and one of the many unfortunate outcomes of this is the deterioration of privacy. Bitcoin fixes this. 
             <br/>
             <br/>
             In much the same way the internet revolutionized the transfer, storage, and communication of data, bitcoin is revolutionizing the transfer, storage, and communication of value. There is nothing grander or more captivating going on in the world today, yet 99% seem to remain oblivious or perplexed. 
@@ -87,10 +86,11 @@ const AboutMe = () => {
             Saddle up.
             </p>
         </div>
-            <div className="mx-auto w-full flex flex-col items-center">
-  <figure className="p-8 bg-slate-200 mb-3 z-99 mt-10">
-            <Image src={beachwalk} />            
-            </figure>
+            <div className="mx-auto w-full md:w-1/2 xl:w-1/3 mb-24 flex flex-col items-center">
+                <figure className="p-8 bg-slate-200 mb-3 z-99 mt-10">
+                <Image src={beachwalk}
+                className="shadow-slate-800 shadow-lg"/>
+                </figure>
             <figcaption className="font-light italic text-slate-600">some beach, somewhere</figcaption>
   </div>
   </div>
