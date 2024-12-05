2bb4b1a388da2b44a06b3318de4a4f94a24c3611
marc <wade.mckirahan@gmail.com>
2024-12-05 14:01:04 -0600

    add a space in the about me component

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,7 +4,6 @@ import beachwalk from "../images/beachwalk.jpg"
 import Image from "next/image"
 
 const AboutMe = () => {
-    
   return (
       <div className="min-h-screen p-4 sm:p-8 sm:py-36 py-36 flex flex-col items-left z-99">
 
