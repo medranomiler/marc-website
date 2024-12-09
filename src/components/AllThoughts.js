beea3f116c97a7862fa5b3b71ab55c2d40b3193d
marc <wade.mckirahan@gmail.com>
2024-12-09 16:51:35 -0600

    Website Update

--- src/components/AllThoughts.js
+++ src/components/AllThoughts.js
@@ -9,7 +9,7 @@ const AllThoughts = () => {
 <>
   <div className="max-w-screen py-36 p-4 relative flex flex-col items-center lg:mt-24 lg:ml-16 lg:mr-16 aboutMe">
       <h1 className="font-bold text-3xl sm:text-4xl mb-16 text-slate-700">I sometimes publish my thoughts on Substack</h1>
-    {loading ? <p>Click here to view my Substack</p> : (<>
+    {loading ? <p>Click here to visit my Substack</p> : (<>
         {Object.keys(thoughtData).length > 0 && thoughtData.map((thought) => {
           return (
             <div key={thought.name} className="min-w-[300px] md:min-w-[600px] max-w-1/2 flex justify-between my-4">
