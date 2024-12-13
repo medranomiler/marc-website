f78438cfe9a8dd069e3a6cafc22e987d28688f43
marc <wade.mckirahan@gmail.com>
2024-12-13 16:23:35 -0600

    Web Update

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,21 +11,21 @@ export default function Home() {
   <div class="flex flex-col text-center mx-auto w-full md:w-1/2 xl:w-1/3 mb-18">
 
     <h1 class="text-left font-bold text-3xl md:text-4xl mt-24 mb-1 text-slate-700">
-    Hello there.
+    Hi I'm Marc
     </h1>
 
-    <p class="leading-8 mt-3 text-xl text-left text-slate-700">
-    My name is Marc.
-
-   </p>
+    <p class="leading-8 mt-8 text-left text-slate-700">
+    Perpetual Student  |  Bitcoin Markets  |  Building the Future of Money    </p>
 
     <p class="leading-8 mt-8 text-left text-slate-700">
-    I am a perpetual student. I like doing things that meet at the intersection of bitcoin, technology, economics, history, and finánce. With a deep-rooted passion for these fields, I enjoy exploring their dynamic relationships and uncovering new insights. As someone who naturally leans against the grain, I possess a unique lens through which I analyze and interpret evolving landscapes and the transformations Bitcoin has on society.
+    I like doing things that meet at the intersection of bitcoin, technology, economics, history, and finánce. With a deep-rooted passion for these fields, I enjoy exploring their dynamic relationships and uncovering new insights. As someone who naturally leans against the grain, I possess a unique lens through which I analyze and interpret evolving landscapes and the transformations Bitcoin has on society.
+
     </p>
-  
-    <p class="leading-8 mt-4 mb-6 text-left text-slate-700">
-    Peek around and connect.
-  </p>
+
+<p class="leading-8 mt-8 text-left text-slate-700">
+Peek around and connect.    
+</p>
+
 </div>
 <div className="mx-auto w-full md:w-1/2 xl:w-1/3 mb-24 flex flex-col items-center">
                 <figure className="p-8 bg-slate-200 mb-3 z-99 mt-10">
